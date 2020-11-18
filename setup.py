b907afcb3cd58fcac14f3cd324b5bff5520e334f
Cameron Anderson <cammy@monolithai.com>
2020-11-18 12:15:45 +0000

    Created inital setup file for pip installation

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+
+setup(
+    name='masters',
+    version='0.1.0',
+    description='Code for masters analysis',
+    url='https://github.com/canderson9756/masters',
+    authour='Cameron Anderson',
+    packages=['STNO_analysis']
+)
